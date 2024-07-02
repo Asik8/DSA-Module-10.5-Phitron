f1556de2ae3897d20384a3091f1c89d9d2fd6d79
Asik8 <motasembillah84@gmail.com>
2024-07-02 22:47:34 +0600

    p3 done using list

--- p3_2.cpp
+++ p3_2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    list<int> l1,l2;
+    list<int> l1;
     int a;
     while(true)
     {
@@ -15,11 +15,9 @@ int main() {
         if(a == -1) break;
         l1.pb(a);
     }
-    bool f = true;
-    while(l1.size()>1)
-    {
-        
-    }
-    cout<<endl;
+    list <int> l2(l1);
+    l2.reverse();
+    if(l1 == l2) cout<<"YES\n";
+    else cout<<"NO\n";
     return 0;
 }
